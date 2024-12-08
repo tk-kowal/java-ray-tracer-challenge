ddd83379364b5231c4c91e61ebe1f2cc4769b994
Tom Kowal <contact.tomk@protonmail.com>
2024-12-08 12:00:28 -0600

    adds all transformations

--- app/src/main/java/org/raytracer/Transform.java
+++ app/src/main/java/org/raytracer/Transform.java
@@ -16,7 +16,6 @@ public class Transform {
                 new float[] { 0, y, 0, 0 },
                 new float[] { 0, 0, z, 0 },
                 new float[] { 0, 0, 0, 1 });
-
     }
 
     public static Matrix rotateX(float radians) {
@@ -24,6 +23,30 @@ public class Transform {
                 new float[] { 1, 0, 0, 0 },
                 new float[] { 0, (float) Math.cos(radians), (float) (-1 * Math.sin(radians)), 0 },
                 new float[] { 0, (float) Math.sin(radians), (float) Math.cos(radians), 0 },
+                new float[] { 0, 0, 0, 1 });
+    }
+
+    public static Matrix rotateY(float radians) {
+        return Matrix.matrix(
+                new float[] { (float) Math.cos(radians), 0, (float) Math.sin(radians), 0 },
+                new float[] { 0, 1, 0, 0 },
+                new float[] { (float) (-1 * Math.sin(radians)), 0, (float) Math.cos(radians), 0 },
+                new float[] { 0, 0, 0, 1 });
+    }
+
+    public static Matrix rotateZ(float radians) {
+        return Matrix.matrix(
+                new float[] { (float) Math.cos(radians), (float) (-1 * Math.sin(radians)), 0, 0 },
+                new float[] { (float) Math.sin(radians), (float) Math.cos(radians), 0, 0 },
+                new float[] { 0, 0, 1, 0 },
+                new float[] { 0, 0, 0, 1 });
+    }
+
+    public static Matrix shear(int xy, int xz, int yx, int yz, int zx, int zy) {
+        return Matrix.matrix(
+                new float[] { 1, xy, xz, 0 },
+                new float[] { yx, 1, yz, 0 },
+                new float[] { zx, zy, 1, 0 },
                 new float[] { 0, 0, 0, 1 });
     }
 
--- app/src/test/java/org/raytracer/TransformTest.java
+++ app/src/test/java/org/raytracer/TransformTest.java
@@ -87,6 +87,96 @@ public class TransformTest {
         var actualQuarter = quarterRotation.tupleMultiply(point);
         assertTrue(Tuple.areEqual(expectedHalfQuarter, actualHalfQuarter));
         assertTrue(Tuple.areEqual(expectedQuarter, actualQuarter));
+    }
 
+    @Test
+    public void test_inverseRotateX() {
+        var point = Point.point(0, 1, 0);
+        var halfQuarterRotation = Transform.rotateX((float) (Math.PI / 4)).inverse();
+        var expectedHalfQuarter = Point.point(0, (float) (Math.sqrt(2) / 2), (float) (-1 * Math.sqrt(2) / 2));
+        var actualHalfQuarter = halfQuarterRotation.tupleMultiply(point);
+        assertTrue(Tuple.areEqual(expectedHalfQuarter, actualHalfQuarter));
+    }
+
+    @Test
+    public void test_rotateY() {
+        var point = Point.point(0, 0, 1);
+        var halfQuarterRotation = Transform.rotateY((float) (Math.PI / 4));
+        var quarterRotation = Transform.rotateY((float) (Math.PI / 2));
+        var expectedHalfQuarter = Point.point((float) (Math.sqrt(2) / 2), 0, (float) (Math.sqrt(2) / 2));
+        var expectedQuarter = Point.point(1, 0, 0);
+        var actualHalfQuarter = halfQuarterRotation.tupleMultiply(point);
+        var actualQuarter = quarterRotation.tupleMultiply(point);
+        assertTrue(Tuple.areEqual(expectedHalfQuarter, actualHalfQuarter));
+        assertTrue(Tuple.areEqual(expectedQuarter, actualQuarter));
+    }
+
+    @Test
+    public void test_rotateZ() {
+        var point = Point.point(0, 1, 0);
+        var halfQuarterRotation = Transform.rotateZ((float) (Math.PI / 4));
+        var quarterRotation = Transform.rotateZ((float) (Math.PI / 2));
+        var expectedHalfQuarter = Point.point((float) (-1 * Math.sqrt(2) / 2), (float) (Math.sqrt(2) / 2), 0);
+        var expectedQuarter = Point.point(-1, 0, 0);
+        var actualHalfQuarter = halfQuarterRotation.tupleMultiply(point);
+        var actualQuarter = quarterRotation.tupleMultiply(point);
+        assertTrue(Tuple.areEqual(expectedHalfQuarter, actualHalfQuarter));
+        assertTrue(Tuple.areEqual(expectedQuarter, actualQuarter));
+    }
+
+    // SHEARING
+
+    @Test
+    public void test_shearXY() {
+        var point = Point.point(2, 3, 4);
+        var transform = Transform.shear(1, 0, 0, 0, 0, 0);
+        var expected = Point.point(5, 3, 4);
+        var actual = transform.tupleMultiply(point);
+        assertTrue(Tuple.areEqual(expected, actual));
+    }
+
+    @Test
+    public void test_shearXZ() {
+        var point = Point.point(2, 3, 4);
+        var transform = Transform.shear(0, 1, 0, 0, 0, 0);
+        var expected = Point.point(6, 3, 4);
+        var actual = transform.tupleMultiply(point);
+        assertTrue(Tuple.areEqual(expected, actual));
+    }
+
+    @Test
+    public void test_shearYX() {
+        var point = Point.point(2, 3, 4);
+        var transform = Transform.shear(0, 0, 1, 0, 0, 0);
+        var expected = Point.point(2, 5, 4);
+        var actual = transform.tupleMultiply(point);
+        assertTrue(Tuple.areEqual(expected, actual));
+    }
+
+    @Test
+    public void test_shearYZ() {
+        var point = Point.point(2, 3, 4);
+        var transform = Transform.shear(0, 0, 0, 1, 0, 0);
+        var expected = Point.point(2, 7, 4);
+        var actual = transform.tupleMultiply(point);
+        assertTrue(Tuple.areEqual(expected, actual));
+    }
+
+    @Test
+    public void test_shearZX() {
+        var point = Point.point(2, 3, 4);
+        var transform = Transform.shear(0, 0, 0, 0, 1, 0);
+        var expected = Point.point(2, 3, 6);
+        var actual = transform.tupleMultiply(point);
+        assertTrue(Tuple.areEqual(expected, actual));
+    }
+
+    @Test
+    public void test_shearZY() {
+        var point = Point.point(2, 3, 4);
+        var transform = Transform.shear(0, 0, 0, 0, 0, 1);
+        var expected = Point.point(2, 3, 7);
+        var actual = transform.tupleMultiply(point);
+        assertTrue(Tuple.areEqual(expected, actual));
     }
 }
